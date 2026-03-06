b682f4335afb4c4d7c6afb1201144ffac30aa127
karnandohuang <karnandohuang@outlook.com>
2026-03-06 22:23:39 +0700

    created GameBoard.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import {Player} from "./components/Player.jsx";
+import {GameBoard} from "./components/GameBoard.jsx";
 
 function App() {
 
@@ -9,7 +10,7 @@ function App() {
                   <Player name={'Player 1'} symbol={'X'} />
                   <Player name={'Player 2'} symbol={'O'} />
               </ol>
-              GAME BOARD
+              <GameBoard />
           </div>
       </main>
   )
---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,35 @@
+import {useState} from "react";
+
+const initialGameBoard = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+]
+
+export function GameBoard() {
+    const [gameBoard, setGameBoard] = useState(initialGameBoard)
+
+    function handleSelectSquare (rowIndex, colIndex) {
+        setGameBoard(prevGameBoard => {
+            const newGameBoard = [...prevGameBoard.map(innerArray => [...innerArray])]
+            newGameBoard[rowIndex][colIndex] = 'X'
+            return newGameBoard
+        })
+    }
+    
+    return (
+        <ol id="game-board">
+            {gameBoard.map((row, rowIndex) => (
+                <li key={rowIndex}>
+                    <ol>
+                        {row.map((playerSymbol, colIndex) =>(
+                            <li key={colIndex}>
+                                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                            </li>
+                        ))}
+                    </ol>
+                </li>
+            ))}
+        </ol>
+    )
+}
